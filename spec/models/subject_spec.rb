fba9eefa9d6fae8e0cc1a075e3f115188c8bd7cb
mostafa <mostafa.k.zahran@gmail.com>
2017-05-12 19:39:58 +0300

    refactor subject spec

--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -1,64 +1,67 @@
 require 'rails_helper'
 
 RSpec.describe Subject, type: :model do
+  before(:each) do
+    @subject = build(:subject)
+  end
+
   it 'class Should exist' do
     expect(Object.const_defined?('Subject')).to be_truthy
   end
 
   it 'should has attribute name' do
-    expect(Subject.new.has_attribute?(:name)).to be_truthy
+    expect(@subject.has_attribute?(:name)).to be_truthy
   end
 
   it 'should has attribute token' do
-    expect(Subject.new.has_attribute?(:token)).to be_truthy
+    expect(@subject.has_attribute?(:token)).to be_truthy
   end
 
   it 'should has attribute mobile_version' do
-    expect(Subject.new.has_attribute?(:mobile_version)).to be_truthy
+    expect(@subject.has_attribute?(:mobile_version)).to be_truthy
   end
 
   it 'should has attribute backend_version' do
-    expect(Subject.new.has_attribute?(:backend_version)).to be_truthy
+    expect(@subject.has_attribute?(:backend_version)).to be_truthy
   end
 
   it 'should has attribute menu_items' do
-    expect(Subject.new.has_attribute?(:menu_items)).to be_truthy
+    expect(@subject.has_attribute?(:menu_items)).to be_truthy
   end
 
   it 'should not create new subject without name' do
-     x =Subject.new(name: nil , mobile_version: '1.0', backend_version: '1.0', token: 'xxxx', menu_items:{test: 'test'})
-     expect(x.save).to be_falsey
+    @subject.name = nil
+    expect(@subject.save).to be_falsey
   end
 
   it 'should not create new subject without mobile_version' do
-    x =Subject.new(name: 'PmP' , mobile_version: nil, backend_version: '1.0', token: 'xxxx', menu_items:{test: 'test'})
-    expect(x.save).to be_falsey
+    @subject.mobile_version = nil
+    expect(@subject.save).to be_falsey
   end
 
   it 'should not create new subject without backend_version' do
-    x =Subject.new(name: 'PmP' , mobile_version: '1.0', backend_version: nil, token: 'xxxx', menu_items:{test: 'test'})
-    expect(x.save).to be_falsey
+    @subject.backend_version = nil
+    expect(@subject.save).to be_falsey
   end
 
   it 'should not create new subject without token' do
-    x =Subject.new(name: 'PmP' , mobile_version: '1.0', backend_version: '1.0', token: nil, menu_items:{test: 'test'})
-    expect(x.save).to be_falsey
+    @subject.token = nil
+    expect(@subject.save).to be_falsey
   end
 
   it 'should not create new subject without menu_items' do
-    x =Subject.new(name: 'PmP' , mobile_version: '1.0', backend_version: '1.0', token: 'xxx', menu_items:{})
-    expect(x.save).to be_falsey
+    @subject.menu_items = {}
+    expect(@subject.save).to be_falsey
   end
 
   it 'should  create new subject with all data not null' do
-    x =Subject.new(name: 'PmP' , mobile_version: '1.0', backend_version: '1.0', token: 'xxx', menu_items:{test: 'test'})
-    expect(x.save!).to be_truthy
+    expect(@subject.save!).to be_truthy
   end
 
   it 'should contain subject' do
     subj = create(:subject)
-    reading_type = ReadingType.create(name: 'type name' )
-    reading = Reading.create(reading_type_id: reading_type.id , header: '1.0', body: '1.0', subject_id: subj.id)
+    reading_type = ReadingType.create(name: 'type name')
+    reading = Reading.create(reading_type_id: reading_type.id, header: '1.0', body: '1.0', subject_id: subj.id)
     expect(subj.readings.first.id == reading.id).to be_truthy
   end
 
