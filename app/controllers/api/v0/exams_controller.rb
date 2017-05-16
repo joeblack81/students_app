d03c53ca945092c2c2e18cb55070702c9a96a05f
Mohamed Masoud <joeblack_8@hotmail.com>
2017-05-16 10:18:35 +0300

    controllers

---
+++ app/controllers/api/v0/exams_controller.rb
@@ -0,0 +1,3 @@
+class Api::V0::ExamsController < ApplicationController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  api_version(:module => "Api::V0", :path => {:value => "api/v0"})do
+    # resources :exams, only:[:index, :create, :update]
+    # resources :questions, only:[:index]
+    # resources :reading, only:[:show]
+  end
+
+
+
+
 end
